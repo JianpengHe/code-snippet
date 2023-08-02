0c450ef7adaf2cfba90038b9d0b4410c17cee3b7
JianpengHe <i@hejianpeng.cn>
2023-08-02 23:53:45 +0800

    fix: 解决报错的问题

--- common/TS按照key精确提示.ts
+++ common/TS按照key精确提示.ts
@@ -1,41 +1,37 @@
 enum EId {
-    A = 1,
-    /** 哈哈哈 */
-    B
+  A = 1,
+  /** 哈哈哈 */
+  B,
 }
 type IReq = {
-    1: { id: number }
-    2: { id2: number }
-}
+  1: { id: number };
+  2: { id2: number };
+};
 type IRes = {
-    1: { name: number }
-    2: { name2: number }
+  1: { name: number };
+  2: { name2: number };
+};
+
+class Test<ReqList, ResList> {
+  public send<Id extends keyof ReqList & keyof ResList>(id: Id, req: ReqList[Id]): Promise<ResList[Id]> {
+    //id=3
+    return new Promise(r => {
+      r(8 as any);
+    });
+  }
+
+  public onRequest<Id extends keyof ReqList & keyof ResList>(
+    id: Id,
+    cb: (res: ResList[Id], req: ReqList[Id]) => void
+  ): void {}
+  public onMessage<Id extends keyof ResList>(id: Id, cb: (res: ResList[Id]) => void): void {}
+
+  public async hookMessage<Id extends keyof ResList>(id: Id, cb: (res: ResList[Id]) => void): Promise<ResList[Id]> {
+    return 0 as any;
+  }
 }
-
-class Test<ReqList, ResList>{
-    public send<Id extends (keyof ReqList & keyof ResList)>(id: Id, req: ReqList[Id]): Promise<ResList[Id]> {
-        //id=3
-        return new Promise(r => {
-            r(8)
-        })
-    }
-
-    public onRequest<Id extends (keyof ReqList & keyof ResList)>(id: Id, cb: (res: ResList[Id], req: ReqList[Id]) => void): void {
-
-    }
-    public onMessage<Id extends keyof ResList>(id: Id, cb: (res: ResList[Id]) => void): void {
-
-    }
-
-    public async hookMessage<Id extends keyof ResList>(id: Id, cb: (res: ResList[Id]) => void): Promise<ResList[Id]> {
-        return 0
-    }
-}
-
 
 const a = new Test<IReq, IRes>();
-a.send(EId.B, { id: 5 });
+a.send(EId.B, { id2: 5 });
 
-a.onRequest(2, (res, req) => {
-
-})
+a.onRequest(2, (res, req) => {});
