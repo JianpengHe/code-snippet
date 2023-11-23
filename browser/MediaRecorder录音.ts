f8ed0cabfb816b8ef633be9b76dd3fd64d0c33b1
JianpengHe <i@hejianpeng.cn>
2023-11-23 22:35:28 +0800

    feat: MediaRecorder录音

---
+++ browser/MediaRecorder录音.ts
@@ -0,0 +1,24 @@
+navigator.mediaDevices
+  .getUserMedia({
+    audio: {
+      sampleRate: 48000,
+      autoGainControl: false,
+      noiseSuppression: false,
+      echoCancellation: false,
+    },
+  })
+  .then(stream => {
+    const chunks: Blob[] = [];
+    const now = new Date().getTime();
+    const mediaRecorder = new MediaRecorder(stream, { mimeType: "audio/webm; codecs=pcm" });
+    mediaRecorder.ondataavailable = ({ data }) => chunks.push(data);
+    mediaRecorder.start();
+    mediaRecorder.onstop = () => {
+      const audioURL = window.URL.createObjectURL(new Blob(chunks, { type: "audio/webm; codecs=pcm" }));
+      const a = document.createElement("a");
+      a.href = audioURL;
+      a.download = now + ".webm";
+      document.body.append(a);
+      a.click();
+    };
+  });
