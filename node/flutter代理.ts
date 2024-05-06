9dc2f1475b9737056f6bf8895adc7747f46a0c04
何健鹏 <i@hejianpeng.cn>
2024-05-06 17:43:14 +0800

    feat: flutter代理

---
+++ node/flutter代理.ts
@@ -0,0 +1,30 @@
+import { socks5 } from "../../tools/dist/node/systemNetworkSettings";
+import { TcpProxy } from "../../tools/dist/node/TcpProxy";
+import { DnsServer } from "../../tools/dist/node/dnsService";
+
+const port = 443;
+const tcpProxy = new TcpProxy(new DnsServer());
+tcpProxy.add({
+  host: "pub.dev",
+  port,
+  connectionListener: sock =>
+    socks5({ host: "pub.dev", port }).then(sock2 => {
+      console.log("代理", "pub.dev", port);
+      sock.pipe(sock2);
+      sock2.pipe(sock);
+      sock.on("error", e => console.log(e));
+      sock2.on("error", e => console.log(e));
+    }),
+});
+tcpProxy.add({
+  host: "maven.google.com",
+  port,
+  connectionListener: sock =>
+    socks5({ host: "maven.google.com", port }).then(sock2 => {
+      console.log("代理", "maven.google.com", port);
+      sock.pipe(sock2);
+      sock2.pipe(sock);
+      sock.on("error", e => console.log(e));
+      sock2.on("error", e => console.log(e));
+    }),
+});
