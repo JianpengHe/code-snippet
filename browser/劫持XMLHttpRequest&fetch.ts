fdc3d9eb235a45f0b372154e4d7d144dab4f35ac
何健鹏 <i@hejianpeng.cn>
2024-06-19 14:15:53 +0800

    feat: fetch支持不同类型

--- browser/劫持XMLHttpRequest&fetch.ts
+++ browser/劫持XMLHttpRequest&fetch.ts
@@ -1,9 +1,16 @@
 import { MyEvent } from "../common/手写事件";
+type IFetchBodyBase = { input: RequestInfo | URL; init?: RequestInit | undefined; eventName: keyof IEventList };
 type IEventList = {
   sendXMLHttpRequest: (xhr: XMLHttpRequest) => void;
   onreadystatechange: (xhr: XMLHttpRequest) => void;
   sendFetch: (info: { input: RequestInfo | URL; init?: RequestInit | undefined }) => void;
-  onFetchBody: (data: any) => void;
+  onFetchBody: (data: { readonly data: any | Promise<any> } & IFetchBodyBase) => void;
+  onFetchArrayBuffer: (data: { data: ArrayBuffer } & IFetchBodyBase) => void;
+  onFetchBlob: (data: { data: Blob } & IFetchBodyBase) => void;
+  onFetchFormData: (data: { data: FormData } & IFetchBodyBase) => void;
+  onFetchJson: (data: { data: { [x: string]: any } } & IFetchBodyBase) => void;
+  onFetchText: (data: { data: string } & IFetchBodyBase) => void;
+  onFetchClone: (data: { readonly data: Response } & IFetchBodyBase) => void;
 };
 
 const XMLHttpRequest = window.XMLHttpRequest;
@@ -51,16 +58,36 @@ export default () => {
     const res = await fetch(info.input, info.init);
     return new Proxy(res, {
       get(target: Response, p: string | symbol) {
-        if (p === "arrayBuffer" || p === "blob" || p === "formData" || p === "json" || p === "text") {
-          return async () => {
-            const body = await res[p]();
-            // // TODO: do any things
-            // console.log(input, body);
-            event.emit("onFetchBody", body);
-            return body;
-          };
+        const emit = async (eventName: keyof IEventList, data?: any) => {
+          data = data ?? (await res[p]());
+          const body = { data, input, init, eventName };
+          // // TODO: do any things
+          // console.log(input, body);
+          event.emit(eventName, body);
+          event.emit("onFetchBody", body);
+          if (body.data instanceof Promise) {
+            body.data = await body.data;
+          }
+          return body.data;
+        };
+        // console.log(target, p);
+        switch (p) {
+          case "arrayBuffer":
+            return emit("onFetchArrayBuffer");
+          case "blob":
+            return emit("onFetchBlob");
+          case "formData":
+            return emit("onFetchFormData");
+          case "json":
+            return emit("onFetchJson");
+          case "text":
+            return emit("onFetchText");
+          case "clone":
+            emit("onFetchClone", target.clone());
+            break;
         }
-        return target[p];
+
+        return typeof target[p] === "function" ? target[p].bind(res) : target[p];
       },
     });
   };
