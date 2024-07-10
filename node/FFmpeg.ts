02985980f8646d35b0be2874d91405e4413b3262
何健鹏 <i@hejianpeng.cn>
2024-07-10 16:24:22 +0800

    feat: 增加完整事件支持

--- node/FFmpeg.ts
+++ node/FFmpeg.ts
@@ -4,7 +4,8 @@ import { RecvStream } from "../../tools/dist/node/RecvStream";
 
 export type IFFmpegEvent = {
   info: (info: IFFmpegInfo) => void;
-  progress: (txt: string, info: IFFmpegInfo) => void;
+  progress: (info: IFFmpegInfo, progress: IFFmpegProgress) => void;
+  end: (info: IFFmpegInfo) => void;
 };
 
 export enum EFFmpegStatus {
@@ -21,9 +22,21 @@ export type IFFmpegStreamInfo = {
   raw: string;
 };
 export type IFFmpegInfo = {
+  durationRaw: string;
+  duration: number;
   input: IFFmpegStreamInfo[][];
   output: IFFmpegStreamInfo[][];
   mapping: string[];
+};
+export type IFFmpegProgress = {
+  bitrate: string;
+  fps: number;
+  frame: number;
+  q: number;
+  speed: string;
+  time: string;
+  raw: string;
+  [x: string]: any;
 };
 
 export class FFmpeg extends MyEvent<IFFmpegEvent> {
@@ -35,22 +48,44 @@ export class FFmpeg extends MyEvent<IFFmpegEvent> {
     this.readBuffer();
   }
   private status: EFFmpegStatus = EFFmpegStatus.info;
+  private info?: IFFmpegInfo;
   private async readBuffer() {
     const buffers: Buffer[] = [];
     let buffer: Buffer | undefined;
-    const changeStatus = () => {
+    const changeStatus = (isEnd = false) => {
       const txt = String(Buffer.concat(buffers)).trim();
       //   console.log([txt], Buffer.concat(buffers));
       switch (this.status) {
         case EFFmpegStatus.info:
-          this.emit("info", FFmpeg.parseInfo(txt));
+          this.info = FFmpeg.parseInfo(txt);
+          this.emit("info", this.info);
           this.status = EFFmpegStatus.progress;
           break;
         case EFFmpegStatus.progress:
-          console.log("changeStatus", txt);
+          const progressInfo: IFFmpegProgress = {
+            bitrate: "",
+            fps: 0,
+            frame: 0,
+            q: 0,
+            speed: "",
+            time: "",
+            raw: txt,
+          };
+          this.info &&
+            this.emit(
+              "progress",
+              this.info,
+              [...txt.matchAll(/([^\s=]+)\s*=\s*([^\s=]+)/g)].reduce((obj, [_, k, v]) => {
+                const value = Number(v);
+                return { ...obj, [k]: isNaN(value) ? v : value };
+              }, progressInfo)
+            );
       }
       //
       buffers.length = 0;
+      if (isEnd && this.info) {
+        this.emit("end", this.info);
+      }
     };
 
     while ((buffer = await this.recvStream.readBufferSync(byte => byte === 13))) {
@@ -59,10 +94,12 @@ export class FFmpeg extends MyEvent<IFFmpegEvent> {
       }
       buffers.push(buffer);
     }
-    changeStatus();
+    changeStatus(true);
   }
   static parseInfo(txt: string) {
     const info: IFFmpegInfo = {
+      duration: 0,
+      durationRaw: "",
       input: [],
       output: [],
       mapping: [],
@@ -103,6 +140,11 @@ export class FFmpeg extends MyEvent<IFFmpegEvent> {
 
       if (i < mappingStart) {
         /** input */
+        if (!info.durationRaw && /^Duration/.test(line)) {
+          info.durationRaw = ((line + ",").match(/Duration:([^,]+),/)?.[1] || "").trim();
+          info.duration = FFmpeg.timeToMilliSecond(info.durationRaw);
+          continue;
+        }
         matchLine(line, info.input);
       } else if (i < outputStart) {
         if (/^Stream /.test(line)) {
@@ -113,68 +155,78 @@ export class FFmpeg extends MyEvent<IFFmpegEvent> {
         matchLine(line, info.output);
       }
     }
-    console.log(JSON.stringify(info, null, 2));
+    // console.log(JSON.stringify(info, null, 2));
     // console.log("----------", lines.slice(inputStart, outputStart));
     // console.log("----------", lines.slice(outputStart));
 
     return info;
   }
+
+  static timeToMilliSecond(time: string) {
+    time = time.trim();
+    let o = 0;
+    const [sec, milliSecond] = time.split(".");
+    if (milliSecond) {
+      o += Number(milliSecond) * Math.pow(10, -1 * milliSecond.length + 3);
+    }
+    sec
+      .split(":")
+      .reverse()
+      .forEach((ch, i) => {
+        o += Number(ch) * Math.pow(60, i) * 1000;
+      });
+    return o;
+  }
 }
 
-// setTimeout(() => {
-//   console.log("手动关闭");
-//   process.exit();
-// }, 500);
-new FFmpeg([
-  "-i",
-  "D:/input.mkv",
-  "-c:v",
-  "hevc_nvenc",
-  "-c:a",
-  "copy",
-  "-rc",
-  "vbr_hq",
-  "-crf",
-  "18",
-  "-tune",
-  "uhq",
-  "-minrate",
-  "414720",
-  "-maxrate",
-  "414720",
-  //   "-hide_banner",
-  "-y",
-  "D:/t6ttt.mkv",
-]);
-// const a = child_process.spawn(
-//   "C:/softwares/ffmpeg.exe",
-//   [
-//     "-i",
-//     "D:/input.mkv",
-//     // "-c:v",
-//     // "hevc_nvenc",
-//     // "-c:a",
-//     // "copy",
-//     // "-rc",
-//     // "vbr_hq",
-//     // "-crf",
-//     // "18",
-//     // "-tune",
-//     // "uhq",
-//     // "-minrate",
-//     // "414720",
-//     // "-maxrate",
-//     // "414720",
-//     // "-hide_banner",
-//     // "-y",
-//     // "D:/t6ttt.mkv",
+// import {
+//   EShowTransferProgressDisplay,
+//   IShowTransferProgressOpt,
+//   ShowTransferProgress,
+// } from "../../tools/dist/node/ShowTransferProgress";
+// const opt: IShowTransferProgressOpt = {
+//   title: "T",
+//   totalSize: 0,
+//   interval: 0,
+//   display: [
+//     // EShowTransferProgressDisplay.瞬间速度,
+//     // EShowTransferProgressDisplay.平均速度,
+//     EShowTransferProgressDisplay.进度条,
+//     EShowTransferProgressDisplay.预估时间,
+//     EShowTransferProgressDisplay.文件名,
 //   ],
-//   { cwd: __dirname }
-// );
-
-// a.stdout.on("data", d => {
-//   console.log("stdout", [d, String(d)]);
-// });
-// a.stderr.on("data", d => {
-//   console.log("stderr", [d, String(d)]);
-// });
+// };
+// const showTransferProgress = new ShowTransferProgress(opt);
+// new FFmpeg([
+//   "-i",
+//   "D:/input.mkv",
+//   //   "-c:v",
+//   //   "hevc_nvenc",
+//   //   "-c:a",
+//   //   "copy",
+//   //   "-rc",
+//   //   "vbr_hq",
+//   //   "-crf",
+//   //   "18",
+//   //   "-tune",
+//   //   "uhq",
+//   //   "-minrate",
+//   //   "414720",
+//   //   "-maxrate",
+//   //   "414720",
+//   //   //   "-hide_banner",
+//   //   "-y",
+//   //   "D:/t6ttt.mkv",
+// ])
+//   .on("info", info => {
+//     showTransferProgress.opt.totalSize = info.duration;
+//     // console.log("info", JSON.stringify(info, null, 2));
+//   })
+//   .on("progress", (info, txt) => {
+//     showTransferProgress.set(FFmpeg.timeToMilliSecond(txt.time));
+//     // console.log("progress", txt, info);
+//   })
+//   .on("end", info => {
+//     showTransferProgress.set(info.duration);
+//     console.log("结束了", info);
+//   });
