ed3ab105c874548c06b8b6daf7568fe2cc104f33
hejianpeng <i@hejianpeng.cn>
2025-07-30 21:04:52 +0800

    feat: 新增setIntervalTimeoutProxy和requestAnimationFrameProxy函数，支持定时器和动画帧的管理

--- browser/backgroundTimer.ts
+++ browser/backgroundTimer.ts
@@ -1,4 +1,8 @@
-(global => {
+export function setIntervalTimeoutProxy(global: any = window) {
+  if (!String(global.setInterval).endsWith("{ [native code] }")) {
+    console.warn("重复加载");
+    return;
+  }
   const workerScript = window.URL.createObjectURL(
     new Blob([
       "(" +
@@ -94,4 +98,66 @@
   worker.onerror = function (event) {
     console.error(event);
   };
-})(window as any);
+}
+
+export function requestAnimationFrameProxy(
+  fps = 60,
+  onVisible: (event?: any) => void = () => {},
+  global: any = window
+) {
+  if (!String(global.requestAnimationFrame).endsWith("{ [native code] }")) {
+    console.warn("重复加载");
+    return;
+  }
+  setIntervalTimeoutProxy(global);
+
+  const nativeRequest = global.requestAnimationFrame.bind(global);
+
+  const taskMap = new Map<number, FrameRequestCallback>();
+  let idCounter = 0;
+  global.requestAnimationFrame = (rawCallback: FrameRequestCallback): number => {
+    const id = idCounter++;
+    const isVisible = !document.hidden; //document.visibilityState === "visible";
+    const callback = (ts: number = performance.now()) => {
+      if (!taskMap.has(id)) return;
+      taskMap.delete(id);
+      rawCallback(ts);
+    };
+    taskMap.set(id, rawCallback);
+    if (isVisible) {
+      nativeRequest(callback);
+    } else {
+      global.setTimeout(callback, 1000 / fps);
+    }
+
+    return id;
+  };
+
+  global.cancelAnimationFrame = taskMap.delete.bind(taskMap);
+
+  const stopImmediatePropagation = (event?: any) => {
+    if (document.hidden) {
+      for (const k of [...taskMap.keys()]) {
+        const callback = taskMap.get(k);
+        callback && global.requestAnimationFrame(callback);
+        global.cancelAnimationFrame(k);
+      }
+    }
+    onVisible(event);
+    return event?.stopImmediatePropagation();
+  };
+  stopImmediatePropagation();
+  for (const event of [
+    "visibilitychange",
+    "mozvisibilitychange",
+    "msvisibilitychange",
+    "webkitvisibilitychange",
+    "qbrowserVisibilityChange",
+    "pagehide",
+    "pageshow",
+  ]) {
+    window.addEventListener(event, stopImmediatePropagation, true);
+    document.addEventListener(event, stopImmediatePropagation, true);
+    document.body.addEventListener(event, stopImmediatePropagation, true);
+  }
+}
